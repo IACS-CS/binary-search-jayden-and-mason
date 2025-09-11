9040e6d1d7d5d3d570b7eb6b83e0ba9bbf7dbfb8
MJ-2009 <januskiewiczmason60@gmail.com>
2025-09-11 18:40:45 +0000

    we need to do work

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import { TextInterface } from "text-interface";
 
 let app = document.querySelector("#app");
 // Create a new "Text Interface"
-let ti = new TextInterface(app, "Example Text Interface");
+let ti = new TextInterface(app, "Guess my naruto char");
 let name = await ti.prompt("What is your name?");
 ti.output("Hello, " + name);
 
