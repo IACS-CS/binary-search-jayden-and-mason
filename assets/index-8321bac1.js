2533b90294f5156606f4a99c377c60c87302afc8
MJ-2009 <MJ-2009@users.noreply.github.com>
2025-09-15 13:22:59 +0000

    deploy: 4e3b9ec29fc706fa61e3d4ba6cc91c0df030f001

--- assets/index-8321bac1.js
+++ assets/index-8321bac1.js
@@ -197,6 +197,15 @@ class a {
 
 let app = document.querySelector("#app");
 // Create a new "Text Interface"
-let ti = new a(app, "Guess my naruto char");
+let ti = new a(app, "Guess my naruto charecter");
 let name = await ti.prompt("What is your name?");
 ti.output("Hello, " + name);
+ti.output("Welcome to the Naruto character guessing game!");
+ti.output("you will pick a charecter from the list below and I will try to guess it");
+// add image of naruto charecters and add a list of thier names 
+ti.output(" are they part of the main cast?");
+let maincast = await ti.prompt("are they part of the main cast?");
+if(maincast.toLocaleLowerCase()=== "yes"){
+let malefemale1= await ti.prompt("are they male or female");
+if(malefemale1.toLocaleLowerCase()==="male");
+} else if(maincast.toLocaleLowerCase()=== "no") ;
