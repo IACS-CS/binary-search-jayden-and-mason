df87d0ed2677a86f153eefd9ce88917cf58f3daf
MJ-2009 <MJ-2009@users.noreply.github.com>
2025-09-17 16:28:24 +0000

    deploy: 3d1fe10e2b4c81d2740d76799dc57b12bbc6d9e1

--- assets/index-b6f13cdf.js
+++ assets/index-b6f13cdf.js
@@ -203,9 +203,21 @@ ti.output("Hello, " + name);
 ti.output("Welcome to the Naruto character guessing game!");
 ti.output("you will pick a charecter from the list below and I will try to guess it");
 // add image of naruto charecters and add a list of thier names 
-ti.output(" are they part of the main cast?");
 let maincast = await ti.prompt("are they part of the main cast?");
 if(maincast.toLocaleLowerCase()=== "yes"){
-let malefemale1= await ti.prompt("are they male or female");
-if(malefemale1.toLocaleLowerCase()==="male");
+  let malefemale1= await ti.prompt("are they male or female");
+  if(malefemale1.toLocaleLowerCase()==="male"){
+let Dojornodoj1= await ti.prompt("do they have a Dojutsu");
+  if(Dojornodoj1.toLocaleLowerCase()==="yes"){
+      let ognoog1= await ti.prompt ("Are they from OG or Ship");
+      if(ognoog1.toLocaleLowerCase()=== "og"){
+        let uchiha1 = await ti.prompt ("Is your character part of the Uchiha?");
+        if(uchiha1.toLocaleLowerCase()==="yes"||uchiha1.toLocaleLowerCase()==="uchiha"){
+          let rook1 = await ti.prompt ("Is your character part of the Rookie 9?");
+          if(rook1.toLocaleLowerCase()==="yes"){ ti.output ("Your character is Sasuke");
+          } else if (rook1.toLocaleLowerCase()==="no") ti.output ("Your character is Itachi");
+        }
+      }
+    }
+  }
 } else if(maincast.toLocaleLowerCase()=== "no") ;
