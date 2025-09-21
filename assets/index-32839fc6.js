c37ecbb75322c3b7d740aac983396f04eb103eb5
disrupter109 <disrupter109@users.noreply.github.com>
2025-09-21 22:44:59 +0000

    deploy: da1ee7d4889ff89e9e3965a0c9292e06f122d730

--- assets/index-32839fc6.js
+++ assets/index-32839fc6.js
@@ -200,6 +200,7 @@ let app = document.querySelector("#app");
 let ti = new a(app, "Guess my naruto charecter");
 let name = await ti.prompt("What is your name?");
 ti.output("Hello, " + name);
+if (name.toLocaleLowerCase()==="the sage of six paths"||name.toLocaleLowerCase()==="sage of six paths") ti.output("Sage of the Six Paths thank you for being the goat and starting Ninshu");
 ti.output("Welcome to the Naruto character guessing game!");
 ti.output("you will pick a charecter from the list below and I will try to guess it");
 // add image of naruto charecters and add a list of thier names 
@@ -257,6 +258,87 @@ let Dojornodoj1= await ti.prompt("do they have a Dojutsu");
         }
         }
       } 
+    } else if (ognoog2.toLocaleLowerCase()==="ship"||ognoog2.toLocaleLowerCase()==="shippuden"){ ti.output("Your character is Sai the PainterBoy");
     }
   }
-} else if(maincast.toLocaleLowerCase()=== "no") ;}
+} else if (malefemale1.toLocaleLowerCase()==="female"){
+  let dojornodoj2= await ti.prompt ("Does your character have dojutsu?");
+  if (dojornodoj2.toLocaleLowerCase()==="yes"){ ti.output("Your character is Hinata Hyuga");
+  } else if (dojornodoj2.toLocaleLowerCase()==="no"){
+    let ognoog3= await ti.prompt("Is your character from OG Naruto or Shippuden?");
+    if (ognoog3.toLocaleLowerCase()==="OG"){
+      let Team7and10= await ti.prompt("Is your character from team 10 or team 7? (yes or no)");
+      if (Team7and10.toLocaleLowerCase()==="yes"){
+        let uselessbum= await ti.prompt("Is your character completely useless for the whole series?");
+        if (uselessbum.toLocaleLowerCase()==="yes"){ ti.output("Your character is Sakura");
+        } else if (uselessbum.toLocaleLowerCase()==="no"){ ti.output("Your character is Ino Yamanaka");
+        }
+      } else if (Team7and10.toLocaleLowerCase()==="no"){
+        let sandvillage= await ti.prompt ("Are they from the Sunagakure (Sand Village)?");
+        if (sandvillage.toLocaleLowerCase()==="yes"){ ti.output("Your character is Temari");
+        } else if (sandvillage.toLocaleLowerCase()==="no"){ ti.output ("Your character is the character who got less screen time than a literal tree, Tenten");
+      }
+      }
+    } else if (ognoog3.toLocaleLowerCase()==="Ship"||ognoog3.toLocaleLowerCase()==="Shippuden"){ ti.output("Your character is Kushina Uzumaki");
+    }
+}
+}
+} else if(maincast.toLocaleLowerCase()=== "no") {
+ let malefemale2= await ti.prompt("Is your character male or female?");
+  if(malefemale2.toLocaleLowerCase()==="male"){
+let Dojornodoj3= await ti.prompt("Does your character have Dojutsu?");
+  if(Dojornodoj3.toLocaleLowerCase()==="yes"){
+      let uchiha3= await ti.prompt("Is your character from the Uchiha Clan?");
+      if(uchiha3.toLocaleLowerCase()==="yes"){
+        let bestgenjutsu= await ti.prompt("Is your character the best genjutsu user of all time?");
+        if (bestgenjutsu.toLocaleLowerCase()==="yes"){ ti.output("Your character is Shisui Uchiha the Teleporter (and the one with Kotoamatsukami)");
+        } else if (bestgenjutsu.toLocaleLowerCase()==="no"){
+          let rinnegan= await ti.prompt("Does your character possess the Rinnegan?");
+          if (rinnegan.toLocaleLowerCase()==="yes"){ ti.output("Your character is Madara Uchiha, one of the founders of Konoha");
+          } else if (rinnegan.toLocaleLowerCase()==="no"){ ti.output("Your character is Fugaku Uchiha");
+        } 
+        }
+      } else if (uchiha3.toLocaleLowerCase()==="no"){
+        let hyugaside= await ti.prompt("Is your character part of the Hyuga Clan?");
+        if (hyugaside.toLocaleLowerCase()==="yes"){
+          let father= await ti.prompt("Is your character a father?");
+          if (father.toLocaleLowerCase()==="yes"){ ti.output("Your character is Hiashi Hyuga");
+          } else if (father.toLocaleLowerCase()==="no"){ ti.output("Your character is Tokuma Hyuga");
+        }
+        } else if (hyugaside.toLocaleLowerCase()==="no"){
+          let Otsutsuki1= await ti.prompt("Is your character part of the Ōtsutsuki Clan?");
+          if (Otsutsuki1.toLocaleLowerCase()==="yes"){
+            let sharbyak= await ti.prompt("Does your character have the Sharingan or the Byakugan?");
+            if (sharbyak.toLocaleLowerCase()==="sharingan"){ ti.output("Your character is Indra Ōtsutsuki");
+            } else if (sharbyak.toLocaleLowerCase()==="byakugan"){ ti.output("Your character is Hamura Ōtsutsuki");
+          }
+            }
+        }
+      }
+    } else if (Dojornodoj3.toLocaleLowerCase()==="no"){
+      let akatsuki1= await ti.prompt("Is your character part of the Akatsuki?");
+      if (akatsuki1.toLocaleLowerCase()==="yes"){
+        let zomcomandzetsu= await ti.prompt("Is your character part of the 'Zombie Combo' (Hidan and Kakuzu) or are they Zetsu? (either one is a yes)");
+        if (zomcomandzetsu.toLocaleLowerCase()==="yes"){
+          let zomcom= await ti.prompt("Is your character part of the Zombie Combo");
+          if (zomcom.toLocaleLowerCase()==="yes"){
+            let jashin= await ti.prompt("Is your character a follower of Jashin?");
+            if (jashin.toLocaleLowerCase()==="yes"){ ti.output("Your character is Hidan");
+            } else if (jashin.toLocaleLowerCase()==="no"){ ti.output("Your character is Kakazu (the money launderer)");
+          }
+        } else if (zomcom.toLocaleLowerCase()==="no"){ ti.output("Your character is Zetsu (White+Black Zetsu Akatsuki Form)");
+        }
+        } else if (zomcomandzetsu.toLocaleLowerCase()==="no"){
+          let artduo= await ti.prompt("Is your character part of the art duo (Sasori and Deidara)?");
+          if (artduo.toLocaleLowerCase()==="yes"){
+            let explosion= await ti.prompt("Does your character think art is an explosion?");
+            if (explosion.toLocaleLowerCase()==="yes"){ ti.output("Your character is Deidara");
+            } else if (explosion.toLocaleLowerCase()==="no"){ ti.output("Your character is Sasori (the bum who lost to Sakura because of plot armor hahahaaa)");
+          }
+          } else if (artduo.toLocaleLowerCase()==="no"){ ti.output("Your character is Kisame Hoshigaki (Might Guy doesnt even remember bro lol)");
+          }
+        }
+      }
+    }
+  }
+}
