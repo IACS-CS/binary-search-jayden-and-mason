ba86441a20a0eba2084f60badcbee2abad1eedda
disrupter109 <disrupter109@users.noreply.github.com>
2025-09-21 16:04:13 +0000

    deploy: 436f700045bd17cdfe6a1a4aa411f03571714697

--- assets/index-89871054.js
+++ assets/index-89871054.js
@@ -216,8 +216,47 @@ let Dojornodoj1= await ti.prompt("do they have a Dojutsu");
           let rook1 = await ti.prompt ("Is your character part of the Rookie 9?");
           if(rook1.toLocaleLowerCase()==="yes"){ ti.output ("Your character is Sasuke");
           } else if (rook1.toLocaleLowerCase()==="no") ti.output ("Your character is Itachi");
+        } else if (uchiha1.toLocaleLowerCase()==="no");let hyuga= await ti.prompt ("do they have the Byakugan");
+        if (hyuga.toLocaleLowerCase()==="yes"){
+          ti.output("Your character is Neji");
+        } else if (hyuga.toLocaleLowerCase()=== "no") {
+          ti.output("Your character is Kakashi");
         }
+      } else if (ognoog1.toLocaleLowerCase()==="ship"||ognoog1.toLocaleLowerCase()==="shippuden"){
+        let uchiha2 = await ti.prompt ("Is your character part of the Uchiha");
+        if (uchiha2.toLocaleLowerCase()==="yes"){ ti.output ("Your character is Tobi/Obito");
+        } else if (uchiha2.toLocaleLowerCase()==="no") ti.output ("Your character is Pain/Nagato");//ai helped us with a minor mistake after the to.localelowercase -->()
       }
+    } else if (Dojornodoj1.toLocaleLowerCase()==="no"){
+      let ognoog2= await ti.prompt ("Are they from OG or Ship");
+      if(ognoog2.toLocaleLowerCase()=== "og"){
+       let sensei1= await ti.prompt ("Is your character a sensei (includes Konohamaru)?");
+        if (sensei1.toLocaleLowerCase()==="yes"){
+         let perv= await ti.prompt ("Is your character a pervy old man (or a pervy young man)?");
+         if (perv.toLocaleLowerCase()==="yes"){
+           let restinpeace= await ti.prompt ("Does your character die?");
+           if (restinpeace.toLocaleLowerCase()==="yes"){ ti.output("Your character is Jiraiya the Gallant");
+           } else ti.output ("Your character is Konohamaru");
+         } else if (perv.toLocaleLowerCase()==="no"){
+          let restinpeace2= await ti.prompt ("Does your character die?");
+          if (restinpeace2.toLocaleLowerCase()==="yes"){ ti.output("Your character is Asumaaaaa (goat)");
+          } else if (restinpeace2.toLocaleLowerCase()==="no"){ ti.output("Your character is Orochimaru");
+          }
+         }
+      } else if (sensei1.toLocaleLowerCase()==="no"){
+        let jinchuriki1= await ti.prompt("Is your character a Jinchuriki?");
+        if (jinchuriki1.toLocaleLowerCase()==="yes"){
+          let hokage1=await ti.prompt("Is your character a Hokage of Konoha?");
+          if (hokage1.toLocaleLowerCase()==="yes"){ ti.output("Your character is Naruto Uzumaki");
+          } else if (hokage1.toLocaleLowerCase()==="no"){ ti.output ("Your character is Gaara of the Sand");
+        }
+        } else if (jinchuriki1.toLocaleLowerCase()==="no"){
+          let taijutsu1= await ti.prompt("Does your character use taijutsu?");
+          if (taijutsu1.toLocaleLowerCase()==="yes"){ ti.output("Your character is Rock Lee");
+          } else if (taijutsu1.toLocaleLowerCase()==="no"){ ti.output("Your character is THE GOAT Shikamaru");
+        }
+        }
+      } 
     }
   }
-} else if(maincast.toLocaleLowerCase()=== "no") ;
+} else if(maincast.toLocaleLowerCase()=== "no") ;}
